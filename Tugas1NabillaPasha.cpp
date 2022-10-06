a683b0aaab2184e96a3a4805e152cefa6113ac2b
1BNabillaPashaMarcellina <114641472+1BNabillaPashaMarcellina@users.noreply.github.com>
2022-10-06 14:09:29 +0700

    Upload tugas

---
+++ Tugas1NabillaPasha.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << " NPM : 2210631170091" << endl;
+    cout << " Umur : 18" << endl;
+    cout << " Nama : Nabilla Pasha Marcellina" << endl;
+    cout << " Kelas : 1B" << endl;
+    cout << " Hobi : Olahraga" << endl;
+    cout << " Cita-cita : Dokter Bedah" << endl;
+
+    return 0;
+}
